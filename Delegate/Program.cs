2c4e2e6fc68face6911c0504891394d8a3f687c2
Matthias-254 <lannoo.matthias@hotmail.com>
2024-09-25 21:00:18 +0200

    Delegate bijgewerkt

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace Delegate
 {
-    delegate void Alarm();
+
+    public delegate void AlarmOptie();
 
     internal class Program
     {
@@ -21,9 +22,70 @@
                 Console.WriteLine("Q om te stoppen.");
                 Console.Write("keuze: ");
                 keuze = Console.ReadLine().ToUpper()[0];
-                Console.WriteLine(keuze + "\n");
+                Console.WriteLine();
 
-            } while (true);
+                AlarmOptie alarmOptie = null;
+
+                switch (keuze)
+                {
+                    case 'T':
+                        alarmOptie = TijdInstellenMenu;
+                        break;
+                    case 'S':
+                        alarmOptie = SluimertijdMenu;
+                        break;
+                    case 'A':
+                        alarmOptie = AlarmMenu;
+                        break;
+                    case 'M':
+                        alarmOptie = WekMethodeMenu;
+                        break;
+                    case 'Q':
+                        Console.WriteLine("Programma wordt afgesloten...");
+                        break;
+                    default:
+                        Console.WriteLine("Ongeldige keuze, probeer opnieuw.\n");
+                        break;
+                }
+
+                if (alarmOptie != null)
+                {
+                    alarmOptie();
+                }
+
+            } while(keuze != 'Q');
+        }
+
+        static void TijdInstellenMenu()
+        {
+            Console.WriteLine("Tijd dat u gewekt wil worden:");
+            string keuze = Console.ReadLine();
+            Console.WriteLine($"Wekker gaat af om {keuze}\n");
+        }
+
+        static void SluimertijdMenu()
+        {
+            Console.WriteLine("Lengte sluimertijd (in minuten):");
+            string keuze = Console.ReadLine();
+            Console.WriteLine($"De sluimertijd staat ingesteld op {keuze} minuten.\n");
+        }
+
+        static void AlarmMenu()
+        {
+            Console.Write("Druk enter om de snooze te starten: ");
+            string wacht = Console.ReadLine();
+            Console.WriteLine("Snooze gestart.\n");
+        }
+
+        static void WekMethodeMenu()
+        {
+            Console.WriteLine("Manier van wekken:");
+            Console.WriteLine("1. Standaard alarmgeluid");
+            Console.WriteLine("2. Rustige muziek");
+            Console.WriteLine("3. Luide pieptoon");
+            Console.Write("Keuze: ");
+            string keuze = Console.ReadLine();
+            Console.WriteLine($"Wekmethode {keuze} gekozen.\n");
         }
     }
 }
