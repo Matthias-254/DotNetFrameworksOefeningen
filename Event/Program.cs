96954027f462483abcc283e6a2da5f810380f0c5
Matthias-254 <lannoo.matthias@hotmail.com>
2024-09-30 15:24:05 +0200

    Event project aangemaakt

---
+++ Event/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+
+        public class Stapel<T>
+        {
+            private Stack<T> stack;
+
+            public Stapel()
+            {
+                Leegmaken();
+            }
+
+            public void Toevoegen(T item)
+            {
+                stack.Push(item);
+            }
+
+            public T Verwijderen()
+            {
+                return stack.Pop();
+            }
+
+            public void Leegmaken()
+            {
+                stack = new Stack<T>();
+            }
+
+            public override string ToString()
+            {
+                string s = "";
+                foreach (T item in stack)
+                {
+                    s += item.ToString() + "\n";
+                }
+                return s;
+            }
+        }
+    }
+}
