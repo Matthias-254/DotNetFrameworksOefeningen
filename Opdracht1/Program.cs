2ed4a89798933960eaaaae59ac8fd581b4d586e2
Matthias-254 <lannoo.matthias@hotmail.com>
2024-10-04 13:54:50 +0200

    Opdracht1 aangemaakt + ef aangepast

--- Opdracht1/Program.cs
+++ Opdracht1/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace KennismakingEF
+﻿namespace Opdracht1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dit is Entity Frameworks");
+            Console.WriteLine("opdracht 1");
         }
     }
 }
