09333a0606f59c5d97ede3ac35a1e7db39c378a9
Matthias-254 <lannoo.matthias@hotmail.com>
2024-10-04 13:49:42 +0200

    EF aangemaakt

---
+++ KennismakingEF/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KennismakingEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
