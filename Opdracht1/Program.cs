8eb7582445fc063e2da34ba34e86f714ebb0f28e
Matthias Lannoo <matthias.lannoo@student.ehb.be>
2024-10-06 16:39:23 +0200

    Klasse tijdschrift toegevoegd

--- Opdracht1/Program.cs
+++ Opdracht1/Program.cs
@@ -1,5 +1,11 @@
 ﻿namespace Opdracht1
 {
+    public enum Verschijningsperiode
+    {
+        Dagelijks,
+        Wekelijks,
+        Maandelijks
+    }
     internal class Program
     {
         static void Main(string[] args)
